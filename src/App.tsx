81d04b25f90d3ffb63ad77f81a0c5b778d2dc921
quitq-dev <quitq.dev@gmail.com>
2023-03-23 21:20:09 +0700

    create UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { CreatePost } from 'pages/blog/components/CreatePost'
+import { PostList } from 'pages/blog/components/PostList'
+
 function App() {
-  return <div className='text-red-600'>hello</div>
+  return (
+    <div className='p-5'>
+      <CreatePost />
+      <PostList />
+    </div>
+  )
 }
 
 export default App
